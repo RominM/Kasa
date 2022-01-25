a24d3753d42f395be7004b22853e7603b5d185ca
RominM <rominmanogil.off@gmail.com>
2022-01-26 00:14:54 +0100

    refacto main in one component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Header from '../../layout/header/Header';
-import Banner from '../../components/banner/Banner';
-import Main from '../../components/main/Main';
-import Footer from '../../layout/footer/Footer';
+import './main.scss';
+import './mediaQ-main.scss';
+import './banner.scss';
+
+const data = require('./../../data/data.json');
 
 const Home = () => {
   return (
-    <React.Fragment>
-      <Banner />
-      <Main />
-    </React.Fragment>
+    <>
+      <div className="banner">
+        <img src={require('./../../assets/background.png')} alt="bannière" />
+        <span className="gimmick">Chez Vous, partout et ailleurs</span>
+      </div>
+      <main className="home-main">
+        <div className="gallery">
+          <ul className="home-main-ul">
+            {data.map((DATA) => (
+              <Link to={'logement/' + DATA.id}>
+                <li className="card" key={DATA.id}>
+                  <img src={DATA.cover} alt={'logement ' + DATA.title} />
+                  <span>{DATA.title}</span>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
   );
 };
 
