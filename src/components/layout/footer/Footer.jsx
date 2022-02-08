aaf36d81ae68c9dbfb89a0e2780037949d79a077
RominM <rominmanogil.off@gmail.com>
2022-02-08 12:15:37 +0100

    project is over

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,14 +1,11 @@
 import './footer.scss';
+import FootLogo from './../../../assets/images/logo_kasa_foot.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <h2>
-        <img
-          className="kasa-logo-foot"
-          src={require('./../../../assets/images/logo_kasa_foot.png')}
-          alt="kasa"
-        />
+        <img className="kasa-logo-foot" src={FootLogo} alt="kasa" />
       </h2>
       <span className="copyright"> &#xA9; 2020 Kasa.All right reserved</span>
     </footer>
